a8fa4650529f69a87c4d6f1bdf19e7da896acbd2
Snorlax1997 <67645446+radovanne3@users.noreply.github.com>
2022-09-02 13:35:09 +0200

    Apply code review advice (#5)
    * Revert "1. Deleted unnecessary comments."
    
    This reverts commit 6d0323c6785042fed99cb22293dbe02f87ef36e6.
    
    * Revert "Odradjenje izmene."
    
    This reverts commit fc018f7231f4f6886e916d9e60c0fd5095e79c27.
    
    * Deleted unnecessary comments;Simplified retract-entity-ids expression ;Increased Thread/sleep time to 15 seconds.
    
    * Changed datomic/query notations from map -> vector (side-effect: needed to change how we assemble conditions for retrieving ads from a db) ; Simplified assemble-data-for-retraction function;
    
    * Code formatting, renaming  and clean-up;
    
    * Code formatting, renaming  and clean-up;
    
    * Extracted queries from functions and created query store vars;
    
    * Email and phone-number are now completely optional (no more "NOT PROVIDED"), one is required to submit a form; Fixed bad aliasing;
    
    * Removed unnecessary function and refactored code;
    
    * Fixed typo;

--- src/clj/kefirnadar/application/routes.clj
+++ src/clj/kefirnadar/application/routes.clj
@@ -1,23 +1,24 @@
 (ns kefirnadar.application.routes
-  (:require [kefirnadar.application.handlers :as h]))
+  (:require [kefirnadar.application.handlers :as h]
+            [spec-tools.data-spec :as ds]))
 
 (def routes
   ["" {:swagger {:consumes ["application/edn" "application/transit+json"]
                  :produces ["application/edn" "application/transit+json"]}}
    ["/list/grains-kind/{ad/grains-kind}/region/{ad/region}"
-    {:get {:handler    h/get-users
+    {:get {:handler    h/get-ads
            :parameters {:path {:ad/region      keyword?
                                :ad/grains-kind keyword?}}}}]
-   ["/create" {:post {:handler    h/create-user
-                      :parameters {:body {:ad/firstname    string?
-                                          :ad/lastname     string?
-                                          :ad/region       keyword?
-                                          :ad/post         boolean?
-                                          :ad/pick-up      boolean?
-                                          :ad/quantity     number?
-                                          :ad/grains-kind  keyword?
-                                          :ad/phone-number string?
-                                          :ad/email        string?}}}}]
-   ["/post-example" {:post {:handler h/get-users}}]
+   ["/create" {:post {:handler    h/create-ad
+                      :parameters {:body {:ad/firstname             string?
+                                          :ad/lastname              string?
+                                          :ad/region                keyword?
+                                          :ad/post                  boolean?
+                                          :ad/pick-up               boolean?
+                                          :ad/quantity              number?
+                                          :ad/grains-kind           keyword?
+                                          (ds/opt :ad/phone-number) string?
+                                          (ds/opt :ad/email)        string?}}}}]
+   ["/post-example" {:post {:handler h/get-ads}}]
    ["/example-delete" {:delete {:handler    (fn [] "implement me!")
                                 :parameters {:body {:db/id number?}}}}]])
