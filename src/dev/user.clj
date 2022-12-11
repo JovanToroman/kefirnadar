172f40b216d063de074073b04a23e373f17d9374
JovanToroman <toromanj@gmail.com>
2022-12-11 16:55:26 +0100

    fix a bug (server was not being started)

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,11 +1,14 @@
 (ns user
-  (:require [kefirnadar.configuration.start :as start]))
+  (:require [kefirnadar.configuration.start :as start]
+            [kefirnadar.configuration.server :as server]))
 
 (defn start []
-  (start/dev-start))
+  (start/dev-start)
+  (server/start-server))
 
 (defn stop []
-  (start/stop))
+  (start/stop)
+  (server/stop-server))
 
 (defn restart []
   (start/dev-restart))
